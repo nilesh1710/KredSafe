3bb2b7e993709a28cd6340fce06c8b7f98e350f3
nilesh1710 <nshekdar@tiuconsulting.com>
2025-10-03 13:47:08 +0530

    Add files via upload

--- tests/loginp.spec.js
+++ tests/loginp.spec.js
@@ -8,21 +8,7 @@ test('Verify Subscription display', async ({ page }) => {
 await page.locator('//input[@name="password"]').fill('Nilesh@2025');
 await page.locator('//*[@id="id_frm_submit"]').click();
 await page.waitForLoadState('networkidle');
- await page.goto('https://dev.kredsafe.net/user/subscription/dashboard');
-await page.waitForLoadState('networkidle');
 
-  // Scroll down function
-  await page.evaluate(() => window.scrollBy(0, window.innerHeight));
-
-  // Grab the subscription values by selectors
-   const subscriptionValue = await page.textContent("span.pill.green");
-  console.log("Subscription - " + subscriptionValue.trim());
-
-  const formsPacketValue = await page.textContent("span.pill.yellow");
-   console.log("Forms and Packet - " + formsPacketValue.trim());
-
-  const totalValue = await page.textContent("span.pill.red");
-   console.log("Total - " + totalValue.trim());
 
   //await browser.close();
 
